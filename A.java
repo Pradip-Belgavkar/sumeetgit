74887d68327fca222984426a1ad17ae7f560242f
Pradip-Belgavkar <pbelgavkar394@gmail.com>
2022-10-16 18:01:20 +0530

    first

---
+++ A.java
@@ -0,0 +1,10 @@
+package com.sumeet;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("A Class");
+	}
+
+}
---
+++ B.java
@@ -0,0 +1,11 @@
+package com.sumeet;
+
+public class B {
+
+	public static void main(String[] args) {
+		
+		System.out.println("B Class");
+
+	}
+
+}
